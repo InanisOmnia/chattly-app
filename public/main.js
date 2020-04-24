d140c99c9893a8795053ecaec6aa801fb97628cd
Michael Grigoryan <56165400+MichaelGrigoryan25@users.noreply.github.com>
2020-04-24 17:47:13 +0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -280,3 +280,9 @@ $(function() {
   });
 
 });
+
+//Loader
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
