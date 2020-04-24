e0f23c920bbc995813f1eae4b71a5c372a77bb7d
Michael <michael.grigoryan25@gmail.com>
2020-04-25 02:07:25 +0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to chattly – ";
+    var message = "Welcome to chattly";
     log(message, {
       prepend: true
     });
