6ede40b4c2002bbbb59994254eff0da7708dbda9
Michael <michael.grigoryan25@gmail.com>
2020-04-26 09:50:21 +0400

    Delete loader to optimise the experience

--- public/main.js
+++ public/main.js
@@ -280,9 +280,3 @@ $(function() {
   });
 
 });
-
-//Loader
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden"; // class "loader hidden"
-});
