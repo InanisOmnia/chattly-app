501182087b859dae5190cc484ba778280059bd50
Michael <michael.grigoryan25@gmail.com>
2020-05-04 15:08:20 +0400

    Minor changes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Setup basic express server
-const express = require('express');
+var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
