ce19af3ba9989e9679e0daeee3031be72b48b4e3
Michael Grigoryan <56165400+MichaelGrigoryan25@users.noreply.github.com>
2020-05-09 19:04:32 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var forceSsl = require('force-ssl-heroku');
-var server = require('https').createServer(app.use(forceSsl));
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
-
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
